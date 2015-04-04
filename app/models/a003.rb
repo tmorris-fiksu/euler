7229ddedc5a20d38812a869a70710429655a8cb2
Trevor Morris <tmorris@fiksu.com>
2015-04-04 17:13:59 -0400

    with puts & implemented

--- app/models/a003.rb
+++ app/models/a003.rb
@@ -1,17 +1,20 @@
 class A003
   def self.prime_factors_of(x)
-    if x ==2
-      return [2]
+    ret = []
+    residual = x
+    (2..Math.sqrt(x)+2).each do |divisor|
+      return ret if divisor > residual
+      puts "pre-while: residual: #{residual}, divisor: #{divisor}"
+      while residual % divisor == 0
+        puts "inside: #{residual} % #{divisor} == 0"
+        residual = residual / divisor
+        ret << divisor
+      end
     end
-    if x == 3
-      return [3]
-    end
-    if x == 4
-      return [2,2]
-    end
+    return ret
   end
 
   def self.run
-    puts "nothing here yet"
+    puts prime_factors_of(600851475143).max
   end
 end
